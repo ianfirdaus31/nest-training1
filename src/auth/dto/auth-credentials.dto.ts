2c6c678efed35229f0bb80a9eb44c3daa7f5732a
Ian Firdaus <ianfirdaus31@gmail.com>
2020-06-14 23:52:59 +0700

    Auth validation & bcrypt package added

---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,13 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
+export class AuthCredentialsDto {
+	@IsString() @MinLength(3) @MaxLength(20)
+	username: string;
+
+	@IsString() @MinLength(5) @MaxLength(20)
+	@Matches(
+		/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/,
+		{ message: 'Password too weak' }
+	)
+	password: string;
+}
